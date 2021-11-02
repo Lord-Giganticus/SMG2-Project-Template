e931d626ebaae653d3500cc44e2316d044279391
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2021-11-01 22:36:28 -0400

    Update LoadResource.h

--- include/spack/Util/LoadResource.h
+++ include/spack/Util/LoadResource.h
@@ -2,6 +2,8 @@
 
 #include "syati.h"
 
+class JKRArchive;
+
 namespace Syati {
 	JKRArchive* loadArchive(const char *pArchivePath);
     void* loadResourceFromArchive(const char *pArchivePath, const char *pFile);
