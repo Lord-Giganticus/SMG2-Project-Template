3897f17be15f1be1180bdf03b1e95806a79ca64e
Lord-Giganticus <mrubins64@gmail.com>
2022-02-03 17:24:42 -0600

    revert

--- source/Extensions/MiscExtensions.cpp
+++ source/Extensions/MiscExtensions.cpp
@@ -147,7 +147,7 @@ namespace SPack {
 
 	void printFileNameIfMissing(const char* fileName) {
 		if (!MR::isFileExist(fileName, 0))
-			//OSPanic("FileRipper.cpp", 118, "File \"%s\" isn't exist.", fileName);
+			OSPanic("FileRipper.cpp", 118, "File \"%s\" isn't exist.", fileName);
 	}
 
 	kmCall(0x804B1FE0, printFileNameIfMissing);
